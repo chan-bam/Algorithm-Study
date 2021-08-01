2c20d45a935db5588576cc7806de47278329e4de
chanbam <exit200@hanmail.net>
2021-08-01 18:56:35 +0900

    210801 수정2

--- swea2056.py
+++ swea2056.py
@@ -6,7 +6,9 @@ for test_case in range(1, T + 1):
 	lst30 =  ['04', '06', '09', '11']
 
 	if 1 <= int(t[4:6]) <= 12:
+
 		if t[4:6] in lst31:
+			
 			if 1 <= int(t[6:8]) <= 31:
 				print(f'#{test_case} {t[0:4]}/{t[4:6]}/{t[6:8]}')
 			else:
@@ -23,7 +25,9 @@ for test_case in range(1, T + 1):
 				print(f'#{test_case} {t[0:4]}/{t[4:6]}/{t[6:8]}')
 			else:
 				print('#', test_case, ' -1', sep='')
+
 		else:
 			print('#', test_case, ' -1', sep='')
+
 	else:
 		print('#', test_case, ' -1', sep='')
